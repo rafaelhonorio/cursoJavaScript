eab11066adb2ba965c5a31ad05ea5776a92e4cd1
Rafael Honório <rafael.reis@invisual.com.br>
2022-03-11 18:03:43 -0300

    Aula de sexta

---
+++ secao8/02_mais_sobre_arrays/script.js
@@ -0,0 +1,10 @@
+let arr = [1, 4, 5, 6, 7]
+let nomes = ['Matheus', 'João', 'Aléxia']
+let bool = [true, false, true]
+
+let misturado = [1, 'Matheus', true]
+
+console.log(arr[1])
+console.log(nomes[0])
+console.log(bool[2])
+console.log(arr[arr.length - 1])

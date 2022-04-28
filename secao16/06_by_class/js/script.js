42ee17ff10bb125b00ac4b5750fc835f423114d7
Rafael Honório <rafael.reis@invisual.com.br>
2022-04-28 14:49:03 -0300

    fim seção 16

---
+++ secao16/06_by_class/js/script.js
@@ -0,0 +1,5 @@
+console.log(document.getElementsByTagName('li'));
+
+console.log(document.getElementsByClassName('items-azuis'));
+
+console.log(document.getElementsByClassName('items-vermelhos'));
---
+++ secao16/12_create_element/js/script.js
@@ -0,0 +1,18 @@
+let lista = document.createElement('ul');
+
+item.appendChild(texto);
+
+for(let i = 0; i < 5; i++){
+    let item = document.createElement('li');
+
+    let texto = document.createTextNode('texto lista' + i);
+
+    item.appendChild(texto);
+
+    lista.appendChild(item);
+    
+}
+
+let container = document.getElementById('container-principal');
+
+container.appendChild(lista);
